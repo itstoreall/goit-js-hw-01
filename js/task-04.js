656396fa22b6a4e7ceeb34f606ecd416ab46fc30
Serhii Stanislav <serhiistanislav@gmail.com>
2020-09-15 21:23:20 +0300

    fixed

--- js/task-04.js
+++ js/task-04.js
@@ -20,3 +20,33 @@ if (totalPrice > credits) {
 console.log(
   `Вы купили ${quantity} дроидов, на счету осталось ${credits} кредитов.`
 );
+
+// const orderPieces = 4;
+
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// const CANCELED_BY_USER = "Отменено пользователем!";
+// const ACCESS_DENIED = "Недостаточно средств на счету!";
+
+// let totalPrice; // Write code on this line
+// let balanceCredit; // Write code on this line
+// let message;
+
+// // Write code under this line
+
+// if (orderPieces === null) {
+//   message = CANCELED_BY_USER;
+// } else {
+//   totalPrice = orderPieces * pricePerDroid;
+// }
+
+// if (totalPrice > credits) {
+//   message = ACCESS_DENIED;
+// } else if (totalPrice <= credits) {
+//   message = `Вы купили ${orderPieces} дроидов, на счету осталось ${
+//     credits - totalPrice
+//   } кредитов`;
+// }
+
+// console.log(totalPrice);
+// console.log(message);
